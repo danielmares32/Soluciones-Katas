b0e27f252b5052459366d45f5ddd129026a99ed7
Daniel Mares <danielmares32@gmail.com>
2022-02-13 13:07:54 -0600

    Se añaden las soluciones

---
+++ open.py
@@ -0,0 +1,16 @@
+from msilib.schema import File
+
+def main():
+	try:
+		#open('/path/to/mars.jpg')
+		#open('config.txt')
+		configuration = open('config.txt')
+	except FileNotFoundError as err:
+		print("Couldn't find the config.txt file!",err)
+	except IsADirectoryError:
+		print("Found config.txt but it is a directory, couldn't read it")
+	except (BlockingIOError, TimeoutError):
+		print("Filesystem under heavy load, can't complete reading configuration file")
+
+if __name__ == '__main__':
+	main()
