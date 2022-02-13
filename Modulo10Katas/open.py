14b9ef4ab9a811a782b9feae47160039260ad571
Daniel Mares <danielmares@pop-os.localdomain>
2022-02-13 14:15:52 -0600

    Se añade la kata 10

--- Modulo10Katas/open.py
+++ Modulo10Katas/open.py
@@ -1,5 +1,3 @@
-from msilib.schema import File
-
 def main():
 	try:
 		#open('/path/to/mars.jpg')
@@ -7,6 +5,8 @@ def main():
 		configuration = open('config.txt')
 	except FileNotFoundError as err:
 		print("Couldn't find the config.txt file!",err)
+	except PermissionError:
+		print("Found config.txt but couldn't read it")
 	except IsADirectoryError:
 		print("Found config.txt but it is a directory, couldn't read it")
 	except (BlockingIOError, TimeoutError):
